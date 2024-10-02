d489d325466214871d146c7dcc5d44e7061b78c6
STUDENT Hannah Warwick <Hannah.Warwick@students.ittralee.ie>
2024-10-02 12:46:56 +0100

    Signed-off-by: STUDENT Hannah Warwick <Hannah.Warwick@students.ittralee.ie>

--- RADZHannah/Assets/movehero.cs
+++ RADZHannah/Assets/movehero.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 
 public class movehero : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+      animator = GetComponent<Animator>(); 
     }
-
+       
     // Update is called once per frame
     void  Update()
 
-    { if (Input.GetKey(KeyCode.W))
-     transform.position += transform.forward * Time.deltaTime;
+    {
+        animator.SetBool("isRunning", false);
+
+
+        if (Input.GetKey(KeyCode.W))
+        { transform.position += transform.forward * Time.deltaTime;
+         animator.SetBool("isRunning", true); }
 
         if (Input.GetKey(KeyCode.A))
             transform.Rotate(Vector3.up, 30* Time.deltaTime);
